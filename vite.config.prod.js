7b023561d364bf22dfecc04e4b0273b82e51767e
dte-84 <dte.solutions.llc@gmail.com>
2026-03-11 03:55:55 -0500

    Fix JSX build for GitHub Pages

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/NestLegacy/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
 })
