f8360795b76e158f60f24ae93b05c80e40dee1dc
dte-84 <dte.solutions.llc@gmail.com>
2026-03-11 03:07:36 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
   },
-  server: {
-    hmr: {
-      overlay: true
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
     }
   }
 })
